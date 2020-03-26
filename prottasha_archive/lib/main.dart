23f24a5ffefccc1409d7fa7dc3c5c6894fa6a6c2
Nayem <ahnayem.sust@gmail.com>
2020-03-26 09:41:10 +0600

    forget pass

--- prottasha_archive/lib/main.dart
+++ prottasha_archive/lib/main.dart
@@ -23,93 +23,105 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 //      appBar: AppBar(
 //        title: Text(widget.title),
 //      ),
-    resizeToAvoidBottomPadding: false,
-      body: Column(
-        children: <Widget>[
-          Container(
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  child: Stack(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.fromLTRB(25.5, 120.0, 0.0, 0.0),
-                        child: Text(
-                          'P',
-                          style: TextStyle(
-                              fontSize: 90.0, fontWeight: FontWeight.bold, color: Colors.indigo,
+        resizeToAvoidBottomPadding: false,
+        body: Column(
+          children: <Widget>[
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.fromLTRB(25.5, 120.0, 0.0, 0.0),
+                          child: Text(
+                            'P',
+                            style: TextStyle(
+                              fontSize: 90.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.indigo,
                               fontStyle: FontStyle.italic,
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.fromLTRB(72, 158.0, 0.0, 0.0),
-                        child: Text(
-                          'rottasha',
-                          style: TextStyle(
-                              fontSize: 45.0, fontWeight: FontWeight.bold, color: Colors.indigo
+                        Container(
+                          padding: EdgeInsets.fromLTRB(72, 158.0, 0.0, 0.0),
+                          child: Text(
+                            'rottasha',
+                            style: TextStyle(
+                                fontSize: 45.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.indigo),
                           ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(72, 198.0, 0.0, 0.0),
-                  child: Text(
-                    'Archive',
-                    style: TextStyle(
-                        fontSize: 20.0, color: Colors.brown
+                        )
+                      ],
                     ),
                   ),
-                )
-              ],
+                  Container(
+                    padding: EdgeInsets.fromLTRB(72, 198.0, 0.0, 0.0),
+                    child: Text(
+                      'Archive',
+                      style: TextStyle(fontSize: 20.0, color: Colors.brown),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 95.0, left: 20.0, right: 20.0),
-            child: Column(
-              children: <Widget>[
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Email/Username',
-                    labelStyle: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green)
-                    )
+            Container(
+              padding: EdgeInsets.only(top: 95.0, left: 20.0, right: 20.0),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: 'Email/Username',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green))),
                   ),
-                ),
-                SizedBox(height: 10.0,),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: "Password",
-                      labelStyle: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green))),
+                    obscureText: true,
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Container(
+                    alignment: Alignment(1.0,0.0),
+                    padding: EdgeInsets.only(top: 15.0, left: 20.0),
+                    child: InkWell(
+                      child: Text(
+                        "Forget Password?",
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Montserrat',
+                            decoration: TextDecoration.underline),
                       ),
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.green)
-                      )
-                  ),
-                  obscureText: true,
-                )
-              ],
-            ),
-          )
-        ],
-      )
-    );
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
